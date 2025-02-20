e2a6cef97e43b9c0e0d6620178f52c73452ca48c
unknown <marcoasamayoac@gmail.com>
2025-02-19 23:30:34 -0600

    last commit Poo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+#include "Cliente.cpp"
 using namespace std;
 
-class Persona{
-    //atributos
-    protected : string nombres, apellidos, direccion;
-                int telefono;
-    //constructor
-    protected :
-            Persona(){
-            }
-            Persona(string nom, string ape, string dir, int tel){
-                nombres = nom;
-                apellidos = ape;
-                direccion = dir;
-                telefono = tel;
-            }
+main(){
+    string nit, nombres, apellidos, direccion;
+    int telefono;
 
-    //metodo
-    void mostrar();
-
-};
+    cout<<"Ingresar Nit: ";
+    cin>>nit;
+    cout<<"Ingresar Nombres: ";
+    cin>>nombres;
+    cout<<"Ingrear Apellidos: ";
+    cin>>apellidos;
+    cout<<"Ingresar Direccion: ";
+    cin>>direccion;
+    cout<<"Ingresar Telefono: ";
+    cin>>telefono;
+    //Instancia de un objeto
+    Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+    obj.mostrar();
+    
+    /*
+    cout<<"Ingresar Nit: ";
+    cin>>nit;
+    obj.setNit(nit);
+    cout<<obj.getNit()<<","<<obj.getNombres()<<","<<obj.getApellidos()<<endl;
+    */
+   
+    /*
+    Cliente obj = Cliente();
+    obj.setNit(nit);
+    obj.setNombres(nombres);
+    obj.setApellido(apellidos);
+    obj.setDireccion(direccion);
+    obj.setTelefono(telefono);
+    obj.mostrar();
+    */
+}
